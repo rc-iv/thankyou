77f97f7468b2f21d2f054cdaa7398c11841b3548
RCIV <rayscook@gmail.com>
2023-03-19 21:07:48 -0400

    switched URL to api gateway url instead of local server

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -70,7 +70,7 @@ const LandingPage = () => {
         console.log(prompt);
         try {
             const response = await axios.post(
-                'http://localhost:5000/api/generate-thank-you',
+                'https://82wpkuvkeh.execute-api.us-east-1.amazonaws.com',
                 null,
                 {params: {prompt}}
             );
