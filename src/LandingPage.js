5d181fb905453f5decf7f85322d05459ba40000f
RCIV <rayscook@gmail.com>
2023-03-20 00:05:54 -0400

    style changes, made gift detail entry 2 columns and wider on medium+ screens

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,7 +1,6 @@
 import React, {useState, useRef} from 'react';
 import Modal from 'react-modal';
 import GiftInput from '../src/components/GiftInput/GiftInput';
-import styles from './LandingPage.module.css';
 import axios from 'axios';
 import OccasionDropdown from './OccasionDropdown';
 import ResponseCard from '../src/components/ResponseCard/ResponseCard';
@@ -108,7 +107,8 @@ const LandingPage = () => {
     };
 
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-blue-100 via-white to-blue-100">
+        <div
+            className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-blue-100 via-white to-blue-100">
             {showResponseCard && (
                 <ResponseCard
                     text={responseText}
@@ -138,64 +138,73 @@ const LandingPage = () => {
                     isOpen={modalIsOpen}
                     onRequestClose={closeModal}
                     contentLabel="Gift Input Modal"
-                    className="relative w-auto p-6 mx-auto mt-10 bg-white rounded shadow-xl outline-none max-w-md"
+                    className="relative w-full p-6 mx-auto mt-10 bg-white rounded shadow-xl outline-none max-w-md md:max-w-3xl"
                     overlayClassName="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
                 >
-                    <div className={styles['modal-content']}>
+                    <div className="max-h-[calc(100vh-200px)] overflow-y-auto pr-2">
                         <div className="w-full">
                             <h2 className="text-2xl font-bold mb-4">Enter Gift Details</h2>
-                            <div className="mb-4">
-                                <label htmlFor="userName" className="block mb-2">
-                                    Your Name:
-                                </label>
-                                <input
-                                    id="userName"
-                                    type="text"
-                                    value={userName}
-                                    onChange={(e) => setUserName(e.target.value)}
-                                    className="w-full px-3 py-2 border border-gray-300 rounded"
-                                    placeholder="Enter your name"
-                                />
-                            </div>
-                            <OccasionDropdown
-                                onOccasionChange={handleOccasionChange}
-                                babyName={babyName}
-                                setBabyName={setBabyName}
-                                giverName={giverName} // Pass giverName as a prop
-                                setGiverName={setGiverName} // Pass setGiverName as a prop
-                            />
-                            <div className="mb-4">
-                                <label htmlFor="giverName" className="block mb-2">
-                                    From:
-                                </label>
-                                <input
-                                    id="giverName"
-                                    type="text"
-                                    value={giverName} // Add value attribute
-                                    onChange={(e) => setGiverName(e.target.value)} // Add onChange attribute
-                                    className="w-full px-3 py-2 border border-gray-300 rounded"
-                                    placeholder="Enter who the gift(s) are from"
-                                />
-                            </div>
+                            <div className="md:flex md:space-x-4">
+                                <div className="mb-4 md:w-1/2">
+                                    <div className="mb-4">
+                                        <label htmlFor="userName" className="block mb-2">
+                                            Your Name:
+                                        </label>
+                                        <input
+                                            id="userName"
+                                            type="text"
+                                            value={userName}
+                                            onChange={(e) => setUserName(e.target.value)}
+                                            className="w-full px-3 py-2 border border-gray-300 rounded"
+                                            placeholder="Enter your name"
+                                        />
+                                    </div>
+                                    <OccasionDropdown
+                                        onOccasionChange={handleOccasionChange}
+                                        babyName={babyName}
+                                        setBabyName={setBabyName}
+                                        giverName={giverName} // Pass giverName as a prop
+                                        setGiverName={setGiverName} // Pass setGiverName as a prop
+                                    />
+                                    {/* Giver Name input */}
+                                    <div className="mb-4">
+                                        <label htmlFor="giverName" className="block mb-2">
+                                            From:
+                                        </label>
+                                        <input
+                                            id="giverName"
+                                            type="text"
+                                            value={giverName} // Add value attribute
+                                            onChange={(e) => setGiverName(e.target.value)} // Add onChange attribute
+                                            className="w-full px-3 py-2 border border-gray-300 rounded"
+                                            placeholder="Enter who the gift(s) are from"
+                                        />
+                                    </div>
+                                </div>
 
-                            <div className="mb-4">
-                                <label htmlFor="numOfGifts" className="block mb-2">
-                                    Number of gifts:
-                                </label>
-                                <select
-                                    id="numOfGifts"
-                                    value={numOfGifts}
-                                    onChange={handleNumOfGiftsChange}
-                                    className="w-full px-3 py-2 border border-gray-300 rounded"
-                                >
-                                    {[...Array(10).keys()].map((i) => (
-                                        <option key={i} value={i + 1}>
-                                            {i + 1}
-                                        </option>
-                                    ))}
-                                </select>
+                                <div className="mb-4 md:w-1/2">
+                                    {/* Number of gifts input */}
+                                    <div className="mb-4">
+                                        <label htmlFor="numOfGifts" className="block mb-2">
+                                            Number of gifts:
+                                        </label>
+                                        <select
+                                            id="numOfGifts"
+                                            value={numOfGifts}
+                                            onChange={handleNumOfGiftsChange}
+                                            className="w-full px-3 py-2 border border-gray-300 rounded"
+                                        >
+                                            {[...Array(10).keys()].map((i) => (
+                                                <option key={i} value={i + 1}>
+                                                    {i + 1}
+                                                </option>
+                                            ))}
+                                        </select>
+                                    </div>
+                                    <GiftInput numOfGifts={numOfGifts} gifts={gifts} setGifts={setGifts}/>
+                                </div>
                             </div>
-                            <GiftInput numOfGifts={numOfGifts} gifts={gifts} setGifts={setGifts}/>
+                            {/* Additional Information input */}
                             <div className="mb-4">
                                 <label htmlFor="additionalInfo" className="block mb-2">
                                     Additional Information:
@@ -234,6 +243,7 @@ const LandingPage = () => {
             )}
         </div>
     );
+
 };
 
 
